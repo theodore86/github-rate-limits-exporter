8a531a2b5d8a02ad6b76afde7af5f8a2b871f37f
Theodoros Georgomanolis <tgeorgomanolis@gmail.com>
2022-12-22 23:20:14 +0200

    feat: add support of rate-limit reset as metric
    Add the x-rate-limit reset header as prometheus metric, it is
    the time which the current rate-limit window resets in UTC epoch
    seconds. Update Grafana sample dashboard to include a table with the
    reset datetime for all the Github API rate-limits.

--- tests/test_collector.py
+++ tests/test_collector.py
@@ -20,11 +20,12 @@ def test_add_metrics(collector, rate_limits_json, mocker):
     expected_metric.add_metric(['github_account','limit'], float(30), CURRENT_TIMESTAMP)
     expected_metric.add_metric(['github_account','used'], float(12), CURRENT_TIMESTAMP)
     expected_metric.add_metric(['github_account','remaining'], float(18), CURRENT_TIMESTAMP)
+    expected_metric.add_metric(['github_account', 'reset'], float(1372697452), CURRENT_TIMESTAMP)
     actual_metric = collector._add_metric(
         api_name='search',
         resources=mock_resources
     )
-    assert mock_unix_timestamp.call_count == 3
+    assert mock_unix_timestamp.call_count == 4
     assert actual_metric == expected_metric
 
 
@@ -49,6 +50,7 @@ def test_collect_metrics(collector, mock_rate_limits, mocker):
     core.add_metric(['github_account','limit'], float(5000), CURRENT_TIMESTAMP)
     core.add_metric(['github_account','used'], float(1), CURRENT_TIMESTAMP)
     core.add_metric(['github_account','remaining'], float(4999), CURRENT_TIMESTAMP)
+    core.add_metric(['github_account', 'reset'], float(1372700873), CURRENT_TIMESTAMP)
 
     search = GaugeMetricFamily(
         'github_rate_limits_search',
@@ -58,6 +60,7 @@ def test_collect_metrics(collector, mock_rate_limits, mocker):
     search.add_metric(['github_account','limit'], float(30), CURRENT_TIMESTAMP)
     search.add_metric(['github_account','used'], float(12), CURRENT_TIMESTAMP)
     search.add_metric(['github_account','remaining'], float(18), CURRENT_TIMESTAMP)
+    search.add_metric(['github_account', 'reset'], float(1372697452), CURRENT_TIMESTAMP)
 
     graphql = GaugeMetricFamily(
         'github_rate_limits_graphql',
@@ -67,6 +70,7 @@ def test_collect_metrics(collector, mock_rate_limits, mocker):
     graphql.add_metric(['github_account','limit'], float(5000), CURRENT_TIMESTAMP)
     graphql.add_metric(['github_account','used'], float(7), CURRENT_TIMESTAMP)
     graphql.add_metric(['github_account','remaining'], float(4993), CURRENT_TIMESTAMP)
+    graphql.add_metric(['github_account', 'reset'], float(1372700389), CURRENT_TIMESTAMP)
 
     integration_manifest = GaugeMetricFamily(
         'github_rate_limits_integration_manifest',
@@ -76,6 +80,7 @@ def test_collect_metrics(collector, mock_rate_limits, mocker):
     integration_manifest.add_metric(['github_account','limit'], float(5000), CURRENT_TIMESTAMP)
     integration_manifest.add_metric(['github_account','used'], float(1), CURRENT_TIMESTAMP)
     integration_manifest.add_metric(['github_account','remaining'], float(4999), CURRENT_TIMESTAMP)
+    integration_manifest.add_metric(['github_account', 'reset'], float(1551806725), CURRENT_TIMESTAMP)
 
     code_scanning_upload = GaugeMetricFamily(
         'github_rate_limits_code_scanning_upload',
@@ -85,6 +90,7 @@ def test_collect_metrics(collector, mock_rate_limits, mocker):
     code_scanning_upload.add_metric(['github_account','limit'], float(500), CURRENT_TIMESTAMP)
     code_scanning_upload.add_metric(['github_account','used'], float(20), CURRENT_TIMESTAMP)
     code_scanning_upload.add_metric(['github_account','remaining'], float(480), CURRENT_TIMESTAMP)
+    code_scanning_upload.add_metric(['github_account', 'reset'], float(1551806725), CURRENT_TIMESTAMP)
 
     expected_metrics = [
         core, search, graphql,
@@ -93,7 +99,7 @@ def test_collect_metrics(collector, mock_rate_limits, mocker):
     ]
 
     assert collector.collect() == expected_metrics
-    assert mock_unix_timestamp.call_count == 15
+    assert mock_unix_timestamp.call_count == 20
     assert mock_rate_limits.call_count == 1
 
 
