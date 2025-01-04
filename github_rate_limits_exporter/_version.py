b6aaabdeefaf037485cfc15b3930728f2f45026a
Theodore Georgomanolis <tgeorgomanolis@gmail.com>
2025-01-04 18:57:28 +0200

    chore(rel): fix pull request action branches

--- github_rate_limits_exporter/_version.py
+++ github_rate_limits_exporter/_version.py
@@ -8,7 +8,7 @@
 __title__ = "github_rate_limits_exporter"
 __description__ = "Prometheus exporter for Github rate-limits API"
 __url__ = "https://github.com/theodore86/github-rate-limits-exporter"
-__version__ = "0.7.4"
+__version__ = "0.7.5"
 __author__ = "Theodore Georgomanolis"
 __author_email__ = "tgeorgomanolis@gmail.com"
 __license__ = "MIT"
